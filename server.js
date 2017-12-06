1f4c8206dd16fbdbc4f4872776e0c3796fdb0b21
miqjan <miqjan@yandex.com>
2017-12-06 22:13:02 +0400

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 app.use(express.static(path.join(__dirname,'static')));
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(__dirname + "static" + '/index.html');
 })
 
